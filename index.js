0b6337918f3f51a8c9287cff9da59ce34c6ceaf7
Shel Orock <sdo243@nyu.edu>
2018-03-28 04:52:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   } else if(string.toLowerCase() === string) {
     return "I can't hear you!"
   } else if(string === "I love you, Grandma.") {
-    return "I love you, too!"
+    return "I love you, too."
   } else {
     return "What, bitch?"
   }
