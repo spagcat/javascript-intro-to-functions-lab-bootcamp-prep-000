5106db1a318781335849a023fb88a673c4dabf6f
Shel Orock <sdo243@nyu.edu>
2018-03-28 04:50:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string.toUpperCase() === string) {
     return "YES INDEED!"
+  } else if(string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
 }
