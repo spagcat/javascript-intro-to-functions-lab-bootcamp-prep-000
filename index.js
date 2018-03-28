744ecf9e0b6db517f38a5cb3ca820cab682be655
Shel Orock <sdo243@nyu.edu>
2018-03-28 04:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,5 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string.toUpperCase) {
     return "YES INDEED!"
-  } else if (string.toLowerCase) {
-    return "I can't hear you!"
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too"
   }
 }
